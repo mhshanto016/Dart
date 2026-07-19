b048b861e95a3402b0a41822c1e8d39d9c659fd9
Mahbobol Hasan Shanto <139173194+mhshanto016@users.noreply.github.com>
2026-07-19 14:11:43 +0600

    Add files via upload

---
+++ oparators.dart
@@ -0,0 +1,40 @@
+void main(){
+  //Addition
+  int a = 5;
+  int b = 3;
+  var add = a+b;
+  print(add);
+
+//Tr div
+int c = 5;
+int d = 2;
+var trdiv = c~/d;
+print(trdiv);
+
+//Multiplication
+int e = 3;
+int f =4;
+var mul = e * f;
+print (mul);
+// Division
+int g = 6;
+int h = 3;
+var div = g/h;
+print (div);
+//Substraction
+int i = 34;
+int j = 32;
+var sub = i-j;
+print (sub);
+String color = 'Green';
+var result = color=='Green'?'Color is Green': 'Color Unknown';
+print (result);
+
+String name = 'Shanto';
+var res = name=='Shanto'?'Name is correct':'Unknown';
+print(res);
+
+
+
+
+}
