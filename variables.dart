494951a794c9f99a842529b46bbcc329ebab6a11
mhshanto016 <139173194+mhshanto016@users.noreply.github.com>
2026-07-19 06:20:37 +0600

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,37 @@
+void main(){
+    int a = 5;
+    int b = 4;
+    int sum = a+b;
+    print (sum);
+    double c= 3;
+    double d = 4;
+    double multiplication= c*d;
+    print(multiplication);
+    List mylist=['Mahbobol','Hasan','Shanto'];
+    print(mylist);
+    List intlist = [12,34,23,43];
+    print (intlist);
+    Map<String, dynamic> Mymap= {
+        'name': 'Shanto',
+        'age': 27,
+    };
+    print(Mymap);
+Map <String, dynamic> mapp = {
+    'name': 'Hasan',
+    'age': 34,
+};
+print(mapp);
+
+Map <String, dynamic> anothermap = {
+    'Name:': 'Karim',
+    'Age': 65,
+};
+print(anothermap);
+
+Runes heart = Runes ('\u{1f49b}');
+    print(String.fromCharCodes(heart));
+
+Runes smilly = Runes('\u{1F604}');
+    print(String.fromCharCodes(smilly));
+}
+
