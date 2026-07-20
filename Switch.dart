31991d0785d8875e27864ca1ccd6859e811464df
Mahbobol Hasan Shanto <139173194+mhshanto016@users.noreply.github.com>
2026-07-20 11:23:46 +0600

    Add files via upload

---
+++ Switch.dart
@@ -0,0 +1,14 @@
+void main(){
+  switch ('Dhaka'){
+    case "Dhaka":
+    print("Correct");
+  }
+switch ('Rajshahi'){
+  case 'Rajshahi':
+  print('Correct Answer');
+}
+switch ('Khulna'){
+  case 'Khulna':
+  print("Correct");
+}
+}
