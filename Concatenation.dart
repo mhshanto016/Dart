da22a71dfaf7a8de16261b75e65dc741a371af05
Mahbobol Hasan Shanto <139173194+mhshanto016@users.noreply.github.com>
2026-07-21 21:30:56 +0600

    Add files via upload

---
+++ Concatenation.dart
@@ -0,0 +1,16 @@
+void main(){
+    String a = 'I';
+    String b = 'Am';
+    String c = 'Mahbobol';
+    String d = 'Hasan';
+    String e = 'Shanto';
+  //  String f = 'I' 'Am' 'Mahbobol' 'Hasan' 'Shanto';
+
+    print(a+b+c+d+e);
+    //or
+    print("$a$b$c$d$e");
+//list type
+List <String> g = ['Shanto', 'Shohag', 'Porag' ];
+print(g.join (' '));
+
+}
